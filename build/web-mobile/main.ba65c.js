ecdff7c062453a63e815966e8c6e2193a24aee8e
xujiawei <xujiawei@puton-inc.com>
2020-05-12 17:48:45 +0800

    发布最新版web-mobil

--- build/web-mobile/main.ba65c.js
+++ build/web-mobile/main.ba65c.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.99278.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.1756d.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.6c77d.js');
+        require('src/settings.b8bd4.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.6c77d.js');
+        require('src/settings.b8bd4.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
